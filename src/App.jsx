1f2e5ca329498654c247786d2e9797275dc8ea5e
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-13 12:35:37 +0530

    add city path to router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import City from "./pages/City";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Home/>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/city' element={<City/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
