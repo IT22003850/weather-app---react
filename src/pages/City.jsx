7375aff840d0ee74d28b78d43608420ff8e2c840
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-14 21:23:45 +0530

    add forecasting functionality to city component

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -6,6 +6,7 @@ const api_key = import.meta.env.VITE_WEATHER_API_KEY;
 
 const City = () => {
   const [newCity, setNewCity] = useState();
+  const [cityForecast, setCityForecast] = useState();
   const [loading, setLoading] = useState(true);
 
   const [searchParams] = useSearchParams();
@@ -33,11 +34,34 @@ const City = () => {
     }
   };
 
+  const getCityForecast = async (lon, lat) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data..!");
+      }
+      const data = await response.json();
+      setCityForecast(data);
+    } catch (error) {
+      console.log(`something went wrong: ${error}`);
+    }
+  };
+
   useEffect(() => {
     if (!city && lon && lat) {
       getCityByCoord(lon, lat);
     }
   }, [city, lon, lat]);
+
+  useEffect(() => {
+    if (lon && lat) {
+      getCityForecast(lon, lat);
+    }
+  }, [lon, lat]);
+
+
 
   return (
     <div>
@@ -48,6 +72,28 @@ const City = () => {
       ) : (
         <CityCard city={newCity} />
       )}
+
+  {cityForecast?.list?.map((city, index) => {
+      const date = new Date(city.dt * 1000);
+      const dayName = date.toLocaleDateString("en-US", { weekday: "short" });
+      const formattedTime = date.toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+
+      return (
+        <div key={index}>
+          <p>{city.main.temp}°C</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+            alt={city.weather[0].description}
+          />
+          <p>{dayName}</p>
+          <p>{formattedTime}</p>
+        </div>
+      );
+    })}
     </div>
   );
 };
