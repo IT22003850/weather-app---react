27313a90e59a5004dbda59bf052251753447020c
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-14 13:20:10 +0530

    changes to search function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,10 @@ const api_key = import.meta.env.VITE_WEATHER_API_KEY;
 
 const Home = () => {
   const [cityName, setCityName] = useState("");
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [triggerSearch, setTriggerSearch] = useState(false);
   const [cities, setCities] = useState([]);
+  const [searchedCity, setSearchedCity] = useState();
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -43,6 +45,9 @@ const Home = () => {
   };
 
   const getCityByName = async (cityName) => {
+    if (!triggerSearch) {
+      return; // Skip useEffect unless fetchTrigger is true
+    }
     try {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}`
@@ -50,10 +55,13 @@ const Home = () => {
       if (!response.ok) {
         throw new Error("Failed to fetch data..!");
       }
-      const data = response.json();
+      const data = await response.json();
+      setSearchedCity(data);
       console.log(data);
     } catch (error) {
       console.log(`something went wrong: ${error}`);
+    } finally {
+      setTriggerSearch(false);
     }
   };
 
@@ -72,7 +80,8 @@ const Home = () => {
   };
 
   const handleSearch = () => {
-    setCityName(searchTerm)
+    setCityName(searchTerm);
+    setTriggerSearch(true);
     console.log(searchTerm);
   };
 
@@ -93,19 +102,34 @@ const Home = () => {
           />
           <button onClick={handleSearch}>Search</button>
         </div>
-        {cities.map((city) => (
-          <ul key={city.id} onClick={() => handleCityClick(city)}>
-            <li>Name: {city.name}</li>
-            <li>Feels Like: {city.main.feels_like}</li>
-            <li>Humidity: {city.main.humidity}</li>
-            <li>Country: {city.sys.country}</li>
-            <li>Icon: {city.weather[0].icon}</li>
+        {!searchedCity &&
+          cities.map((city) => (
+            <ul key={city.id} onClick={() => handleCityClick(city)}>
+              <li>Name: {city.name}</li>
+              <li>Feels Like: {city.main.feels_like}</li>
+              <li>Humidity: {city.main.humidity}</li>
+              <li>Country: {city.sys.country}</li>
+              <li>Icon: {city.weather[0].icon}</li>
+              <img
+                src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+                alt={city.weather[0].description}
+              />
+            </ul>
+          ))}
+
+        {searchedCity && (
+          <ul onClick={() => handleCityClick(searchedCity)}>
+            <li>Name: {searchedCity.name}</li>
+            <li>Feels Like: {searchedCity.main.feels_like}</li>
+            <li>Humidity: {searchedCity.main.humidity}</li>
+            <li>Country: {searchedCity.sys.country}</li>
+            <li>Icon: {searchedCity.weather[0].icon}</li>
             <img
-              src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
-              alt={city.weather[0].description}
+              src={`https://openweathermap.org/img/wn/${searchedCity.weather[0].icon}@2x.png`}
+              alt={searchedCity.weather[0].description}
             />
           </ul>
-        ))}
+        )}
       </header>
     </div>
   );
