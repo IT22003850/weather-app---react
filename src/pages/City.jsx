01f598c03ab7ad152d9753f063bb22989a297d9f
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-14 20:14:52 +0530

    handle null city state when loading component.

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,29 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useSearchParams } from "react-router";
 import CityCard from "../components/CityCard";
 
+const api_key = import.meta.env.VITE_WEATHER_API_KEY;
+
 const City = () => {
+  const [newCity, setNewCity] = useState();
+
   const [searchParams] = useSearchParams();
   const lon = searchParams.get("lon") || "";
   const lat = searchParams.get("lat") || "";
-  console.log(lon, lat);
 
   const location = useLocation();
-  const city  = location.state?.city;
+  const city = location.state?.city || null;
+
+  const getCityByCoord = async (lon, lat) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data..!");
+      }
+      const data = await response.json();
+      setNewCity(data);
+    } catch (error) {
+      console.log(`something went wrong: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    if (!city && lon && lat) {
+      getCityByCoord(lon, lat);
+    }
+  }, [city, lon, lat]);
+
   return (
     <div>
-      {city? <CityCard city={city}/> : <h1>city not available!</h1>}
-      {/* <ul key={city.id} style={{ marginBottom: "1rem" }}>
-        <li>Name: {city.name}</li>
-        <li>Feels Like: {city.main.feels_like}</li>
-        <li>Humidity: {city.main.humidity}</li>
-        <li>Country: {city.sys.country}</li>
-        <li>Icon: {city.weather[0].icon}</li>
-        <img
-          src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
-          alt={city.weather[0].description}
-        />
-      </ul> */}
+      {city ? (
+        <CityCard city={city} />
+      ) : newCity ? (
+        <CityCard city={newCity} />
+      ) : (
+        <p>Loading City data..!</p>
+      )}
     </div>
   );
 };
