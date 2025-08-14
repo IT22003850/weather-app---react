83f1adfeeac6e4514995a5b87ee06b540a6dcc3d
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-08-14 19:36:59 +0530

    handle empty city state

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { useLocation } from "react-router";
+import { useLocation, useSearchParams } from "react-router";
+import CityCard from "../components/CityCard";
 
 const City = () => {
-    const location = useLocation()
-    const {city} = location.state;
+  const [searchParams] = useSearchParams();
+  const lon = searchParams.get("lon") || "";
+  const lat = searchParams.get("lat") || "";
+  console.log(lon, lat);
+
+  const location = useLocation();
+  const city  = location.state?.city;
   return (
     <div>
-      <ul key={city.id} style={{ marginBottom: "1rem" }}>
+      {city? <CityCard city={city}/> : <h1>city not available!</h1>}
+      {/* <ul key={city.id} style={{ marginBottom: "1rem" }}>
         <li>Name: {city.name}</li>
         <li>Feels Like: {city.main.feels_like}</li>
         <li>Humidity: {city.main.humidity}</li>
@@ -16,7 +23,7 @@ const City = () => {
           src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
           alt={city.weather[0].description}
         />
-      </ul>
+      </ul> */}
     </div>
   );
 };
